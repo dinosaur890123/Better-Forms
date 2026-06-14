13188299e650e88541de386dc1552026244fc8fb
dinosaur890123 <dinosaur890123@gmail.com>
2026-06-14 19:30:23 +1000

    Initial file structure and placeholders

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import styles from "./page.module.css";
+export default function Home() {
+  const mockForms = [
+    {id: "feedback", title: "Product feedback survey", responses: 12},
+    {id: "rsvp", title: "Annual event RSVP", responses: 4},
+  ];
+
+  return {
+    
+  }
+}
